e37cfc353ff25c04341edc93990b4286e19852fd
jben0285 <jbennett0285@sdsu.edu>
2024-12-11 13:20:05 -0800

    epic ui
    ui

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -1,28 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
+    public PlayerWeaponController playerWeaponController; // Reference to the weapon controller
+    public TextMeshProUGUI ammoText; // Reference to the TextMeshPro component
 
-    public int ammo;
-
-    public PlayerWeaponController playerWeaponController;
-
-    public int currentAmmoCount;
-    
-    public int totalBulletCount;
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Find the PlayerWeaponController if not already assigned
+        if (playerWeaponController == null)
+        {
+            playerWeaponController = GameObject.FindGameObjectWithTag("WeaponController")
+                .GetComponent<PlayerWeaponController>();
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        currentAmmoCount = playerWeaponController.currentAmmoCount;
+        // Check if the playerWeaponController exists and update the text
+        if (playerWeaponController != null && ammoText != null)
+        {
+            ammoText.text = $"{playerWeaponController.currentAmmoCount}/{playerWeaponController.totalBulletCount}";
+        }
     }
 }
